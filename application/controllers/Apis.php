e90ee98d64f6167487e3a59e11d5e49e24e62ffe
Harsh <harsh.gupta@cognustechnology.com>
2024-05-14 09:41:42 +0530

    Create apis for node projecrt

---
+++ application/controllers/Apis.php
@@ -0,0 +1,154 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Apis extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->load->helper('url');
+        $this->load->helper('form');
+        $this->load->model('ViewsModel');
+
+        $this->metaData = $this->ViewsModel->getSeoDetails('Ipo');
+        $this->upcoming = $this->ViewsModel->getRecentsByType('Upcoming');
+        $this->sme = $this->ViewsModel->getRecentsByType('SME');
+        $this->nav = $this->ViewsModel->nav();
+
+    }
+
+    public function getNav()
+    {
+        $nav['message'] = "success";
+        $nav['result'] = $this->ViewsModel->nav();
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($nav));
+    }
+
+    public function homePage()
+    {
+        $data['message'] = "success";
+        $data['metaData'] = $this->ViewsModel->getSeoDetails('Ipo');
+        $data['upcomingData'] = $this->ViewsModel->getRecentsByType('Upcoming');
+        $data['smeData'] = $this->ViewsModel->getRecentsByType('SME');
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($data));
+    }
+
+    public function gmpData()
+    {
+
+        $data['message'] = "success";
+        $data['metaData'] = $this->ViewsModel->getSeoDetails('greyMarketIpo');
+        $data['gmp'] = $this->ViewsModel->get('gmp');
+        $data['oldGmp'] = $this->ViewsModel->get('old_gmp');
+        $data['faq'] = $this->ViewsModel->getWhere('faq', 'type', 'GMP');
+        $data['faqTitle'] = "Grey Market Premium FAQ's";
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($data));
+    }
+
+
+    public function smeData()
+    {
+        $data['message'] = "success";
+        $data['metaData'] = $this->ViewsModel->getSeoDetails('smeMarketIpo');
+        $data['sme'] = $this->ViewsModel->get('sme');
+        $data['faq'] = $this->ViewsModel->getWhere('faq', 'type', 'SME');
+        $data['faqTitle'] = "SME FAQ's";
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($data));
+    }
+
+    public function subscriptionData()
+    {
+        $data['message'] = "success";
+        $data['metaData'] = $this->ViewsModel->getSeoDetails('subscriptionStatus');
+        $data['ipo_subscription_data'] = $this->ViewsModel->get('ipo_subscription_data');
+        $data['sme_subscription_data'] = $this->ViewsModel->get('sme_subscription_data');
+        $data['faq'] = $this->ViewsModel->getWhere('faq', 'type', 'Status');
+        $data['faqTitle'] = "IPO Subscription FAQ's";
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($data));
+    }
+
+    public function formsData()
+    {
+        $data['message'] = "success";
+        $data['metaData'] = $this->ViewsModel->getSeoDetails('ipoForms');
+        $data['forms'] = $this->ViewsModel->get('forms');
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($data));
+    }
+
+    public function buyBackData()
+    {
+        $data['message'] = "success";
+        $data['metaData'] = $this->ViewsModel->getSeoDetails('sharesBuyBack');
+        $data['buyback'] = $this->ViewsModel->get('buyback');
+        $data['faq'] = $this->ViewsModel->getWhere('faq', 'type', 'Buyback');
+        $data['faqTitle'] = "IPO Buy Back FAQ's";
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($data));
+    }
+
+    public function upcomingIpo()
+    {
+        $data['message'] = "success";
+        $data['metaData'] = $this->ViewsModel->getSeoDetails('upcomingIpo');
+        $data['upcomingIpos'] = $this->ViewsModel->get('ipos');
+        $data['faq'] = $this->ViewsModel->getWhere('faq', 'type', 'upcoming');
+        $data['faqTitle'] = "FAQ's about IPO's";
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($data));
+    }
+
+    public function ipoDetails()
+    {
+
+        $slug = $this->input->get('slug');
+        $slug = $this->db->escape_str($slug);
+        if (!$slug) {
+            $data['message'] = "failed";
+            $data['result'] = "No slug exists";
+
+            return $this->output
+                    ->set_content_type('application/json')
+                    ->set_output(json_encode($data));
+        }
+        $query = $this->db->query("SELECT * FROM details WHERE slug = ?", array($slug));
+        $response = $query->row(); 
+        if (!$response) {
+            $data['message'] = "failed";
+            $data['result'] = "No Result Found";
+
+            return $this->output
+                    ->set_content_type('application/json')
+                    ->set_output(json_encode($data));
+        }
+
+        $tableName = $response->source_table;
+        $link = $response->link;
+        $joinedData = $this->db->query("SELECT * FROM details JOIN $tableName ON details.link = $tableName.link WHERE details.link = ?", array($link));
+
+
+        $data['message'] = "success";
+        $data['result'] = $joinedData->row();
+        $this->output
+        ->set_content_type('application/json')
+        ->set_output(json_encode($data));
+    }
+
+    
+
+}
